4714d580169ebbf9daf62476afe773bdf52913d0
petko.lyutskanov <simona2000petko@abv.bg>
2022-09-23 13:14:56 +0300

    Json kafka producer created

---
+++ src/main/java/com/example/kafkajsonproducer/kafka/JsonKafkaProducer.java
@@ -0,0 +1,30 @@
+package com.example.kafkajsonproducer.kafka;
+
+import com.example.kafkajsonproducer.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+
+public class JsonKafkaProducer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(JsonKafkaProducer.class);
+
+    private KafkaTemplate<String, User> kafkaTemplate;
+
+    public JsonKafkaProducer(KafkaTemplate<String, User> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(User data) {
+        LOGGER.info(String.format("Message sent -> %s", data.toString()));
+
+        Message<User> message = MessageBuilder
+                .withPayload(data)
+                .setHeader(KafkaHeaders.TOPIC, "json")
+                .build();
+
+        kafkaTemplate.send(message);
+    }
+}
